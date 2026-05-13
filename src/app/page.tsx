70711aeb1d0696fa09331e824042ecfbd166291e
Asyllzhan <asilzhan.bakytzhanuly@gmail.com>
2026-05-13 23:22:10 +0500

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         <div
           className="absolute inset-0 bg-cover bg-center scale-105"
           style={{
-            backgroundImage: "url('/aq7.jpeg')",
+            backgroundImage: "url('/aq8.jpeg')",
           }}
         />
 
